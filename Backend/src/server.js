4c17cf0386dcc111418c70e252a103d3502722f9
KirtanThakkar8777 <kirtanthakkar2835@gmail.com>
2026-06-06 12:39:22 +0530

    resolve cors policy error

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -18,7 +18,9 @@ import invoiceRoutes    from '../routers/invoice.routes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/auth',authRoutes);
